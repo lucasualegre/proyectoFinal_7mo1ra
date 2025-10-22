d531961475fafbf63e9d3c623fcf1319429d66b2
Jmaristain100 <21.j.maristain@galileo.edu.ar>
2025-10-22 19:26:17 -0300

    Add files via upload

--- listaReservas.java
+++ listaReservas.java
@@ -1,5 +1,51 @@
-package reserva;
-
+package desarrollo;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.List;
 public class listaReservas {
 
+	
+	List<reservas> reserva = new ArrayList<>();
+
+	public void CrearReserva(int ReservaId, int ClienteId , int HabitacionId, int FechaIngreso, int FechaSalida, int MetodoPago){
+		
+	reservas reserva = new reservas(ReservaId,ClienteId,HabitacionId,FechaIngreso,FechaSalida,MetodoPago);
+	this.reserva.add(reserva);
+
+	
+	}
+	
+	
+	public void AgregarReserva(reservas reserva) {
+		this.reserva.add(reserva);
+	}
+	
+		
+	public void CancelacionReserva (int reservitaId){
+		for (int i=0; i<=reserva.size();i++) {
+			reservas reservita = reserva.get(i);
+			if (reservita.getReservaId()==reservitaId) {
+				reserva.remove(reserva.get(i));
+			}
+		}
+	}
+	
+	public void HistorialReservaClientes(int clienteId) {
+		for (int i=0; i<=reservas.size();i++) {
+			reservas reservita = reserva.get(i);
+			if (reservita.getClienteId()==clienteId)  {
+				System.out.println(reserva);
+			}
+			}
+		}
+	public void TiempoReservado (int ReservaId) {
+		for (int i=0; i<=reservas.size();i++) {
+			reservas reservita = reserva.get(i);
+			if (reservita.getreservaId()==reservaId)  {
+				System.out.println("Tiempo Reservado" +
+				  ChronoUnit.DAYS.between(FechaIngreso, FechaSalida)
+				  + " dias.");
+	}
+		}
 }
+
