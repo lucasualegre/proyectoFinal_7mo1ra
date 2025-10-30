e36d0504fc916aa7823a2ad7614fff4856aa0749
P4nsit089 <g.mollo@galileo.edu.ar>
2025-10-29 21:14:25 -0300

    Add files via upload

--- ReservasActivas.java
+++ ReservasActivas.java
@@ -1,20 +1,24 @@
-package desarrollo;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class ReservasActivas extends ListaReservas {
+public class ReservasActivas extends listaReservas {
 	private int reservis2;
 	public void reservis (int ReservaId){
 		this.reservis=ReservaId;
 	}
-	ListaReservas res = new ListaReservas();
-	public void TiempoReservado(int ReservaId) {
-		super.TiempoReservado(ReservaId);
+	public ReservasActivas(int reservis2){
+		this.reservis2 = reservis2;
+		
 	}
-	List<String> ReservasActivas = new ArrayList();
-	
-	
+	public void preciototal (int habitacionId){
+		for (int i=0;i<lista_de_habitaciones.size();i++){
+			lista_de_habitaciones lis = lista_de_habitaciones.get(i);
+			if (lis.getReservaId()==habitacionId){
+				System.out.println( "Precion total :"+super.gettotaldias()*super.get_costo);
+			}
 
+		}
+		
+	}
 }
